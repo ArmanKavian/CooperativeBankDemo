8f6944701f9d0c21e8cd38f4dc42af874f2d7ad8
Arman Kavian <Arman.Kavian@gmail.com>
2024-11-05 19:26:03 +0100

    Initial project setup with dependencies and configurations

---
+++ src/test/java/com/cobank/CooperativeBankDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperativeBankDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
