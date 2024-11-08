6a15edcd2309c64a039c552cc56855debdb83402
Arman Kavian <Arman.Kavian@gmail.com>
2024-11-08 14:39:55 +0100

    Add database sequence for generating iban.

--- src/main/java/com/cobank/service/iban/IbanService.java
+++ src/main/java/com/cobank/service/iban/IbanService.java
@@ -1,11 +1,11 @@
 package com.cobank.service.iban;
 
-
+import com.cobank.repository.AccountRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.security.SecureRandom;
-import java.util.stream.Collectors;
 
 @Service
 public class IbanService {
@@ -13,7 +13,9 @@ public class IbanService {
     private final String checkDigits;
     private final String bankCode;
     private final int accountNumberLength;
-    private final SecureRandom random = new SecureRandom();
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     public IbanService(
             @Value("${iban.country-code}") String countryCode,
@@ -28,13 +30,16 @@ public class IbanService {
     }
 
     public String generateIban() {
-        String accountNumber = generateRandomNumericString();
+        long sequenceNumber = getNextAccountSequence();
+        String accountNumber = String.format("%0" + accountNumberLength + "d", sequenceNumber);
+
         return "%s%s%s%s".formatted(countryCode, checkDigits, bankCode, accountNumber);
     }
 
-    private String generateRandomNumericString() {
-        return random.ints(accountNumberLength, 0, 10)
-                .mapToObj(String::valueOf)
-                .collect(Collectors.joining());
+    private long getNextAccountSequence() {
+        Query query = entityManager.createNativeQuery(
+                "SELECT NEXT VALUE FOR account_sequence"
+        );
+        return ((Number) query.getSingleResult()).longValue();
     }
 }
